1fc1ffbccf6659c1f9da9bc20b8eb91699f31845
Jany <jany.martelli@shambix.com>
2015-08-06 20:17:13 +0200

    small improvements v.1.3

--- functions.php
+++ functions.php
@@ -4,10 +4,11 @@
 * [WP] Starter Child Theme - SETUP
 * Custom theme that works with its parent [WP] Starter https://github.com/Jany-M/WP-Starter
 * Developed by Shambix @ http://www.shambix.com
-* Version 1
+* Please check for info https://github.com/Jany-M/WP-Starter-Child-Theme
 *
 -------------------------------------------------------------------------------- */
 global $theme_name;
+global $locale;
 
 // ADD THEME SUPPORT
 function wp_starter_childtheme_setup() {
@@ -46,16 +47,16 @@ include_once TEMPLATEPATH.'/library/wordpress/cool_scripts.php';
 -------------------------------------------------------------------------------- */
 function load_child_files() {
 	
-	// Default CSS
-	wp_register_style( 'custom_css', ''.get_stylesheet_directory_uri().'/assets/css/style.css', array('bootstrap_css'), '', 'all');
-	wp_enqueue_style( 'custom_css' );
-
 	// Browser Specific
 	/*global $wp_styles;
 	wp_register_style( 'ie7_css', ''.get_stylesheet_directory_uri().'/assets/css/ie7.css', array('custom_css'), '', screen');
 	$wp_styles->add_data( 'ie7_css', 'conditional', 'IE 7' );
 	wp_enqueue_style( 'ie7_css' );*/
-	
+
+	// Default CSS
+	wp_register_style( 'custom_css', ''.get_stylesheet_directory_uri().'/assets/css/style.css', array('bootstrap_css'), '', 'all');
+	wp_enqueue_style( 'custom_css' );
+
 	// Responsive CSS
 	if(file_exists(get_stylesheet_directory_uri().'/assets/css/responsive.css')) {
 		wp_register_style( 'resp_theme_css', get_stylesheet_directory_uri().'/assets/css/responsive.css', array('custom_css'), '', 'all');
@@ -63,8 +64,10 @@ function load_child_files() {
 	}	
 	
 	// Print CSS
-	/*wp_register_style( 'print_css', ''.get_stylesheet_directory_uri().'/assets/css/print.css', array('resp_theme_css'), '', 'screen');
-	wp_enqueue_style( 'print_css' );*/
+	if(file_exists(get_stylesheet_directory_uri().'/assets/css/print.css')) {
+		wp_register_style( 'print_css', ''.get_stylesheet_directory_uri().'/assets/css/print.css', array('resp_theme_css'), '', 'print');
+		wp_enqueue_style( 'print_css' );
+	}
 
 }
 
@@ -86,5 +89,38 @@ if(!is_admin()) {
 //include(get_stylesheet_directory_uri().'custom/wordpress/custom_meta_boxes.php'); // use this file to add custom meta boxes or edit system ones
 
 // Include Custom scripts & functions
+
+
+/* -------------------------------------------------------------------------------- 
+*
+* [WP] Starter Child Theme - DEBUG
+*
+-------------------------------------------------------------------------------- */
+/*function display_vars() {
+	global $wp_query, $post, $product, $woocommerce, $WCS;
+
+	if(current_user_can('activate_plugins')) : 
+		echo '<pre>';
+		print_r( $wp_query );
+		echo '</pre>';
+
+		echo '<pre>';
+		print_r($post);
+		echo '</pre>';
+
+		echo '<pre>';
+		print_r($product);
+		echo '</pre>';
+
+		echo '<pre>';
+		print_r($woocommerce);
+		echo '</pre>';
+
+		echo '<pre>';
+		print_r($WCS);
+		echo '</pre>';
+	endif;
+}
+add_action('wp','display_vars');*/
 
 ?>
